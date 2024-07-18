689ecd758310ac27f4419d0d4fe1238cc8cdf619
William Schrauwen <williamschrauwen@gmail.com>
2024-07-18 20:50:50 +0200

    fix: names pokemon chart

--- components/PokemonCharts.tsx
+++ components/PokemonCharts.tsx
@@ -140,16 +140,19 @@ export const PokemonCharts = () => {
         </div>
       </fieldset>
 
-      <ResponsiveContainer width="100%" height={500}>
+      <ResponsiveContainer width="100%" height={600}>
         <BarChart
           data={data}
           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="name" angle={-45} textAnchor="end" height={100} />
           <YAxis />
           <Tooltip />
-          <Legend />
+          <Legend
+            verticalAlign="top"
+            wrapperStyle={{ paddingBottom: '20px' }}
+          />
           {selectedAttributes.includes('weight') && (
             <Bar dataKey="weight" fill="#8884d8" />
           )}
